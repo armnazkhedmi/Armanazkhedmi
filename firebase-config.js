cf0de576c2e308cc3602f8ffbe3f71002a121984
armnazkhedmi <armnazkhedmi@gmail.com>
2026-08-21 21:47:27 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// بعد إنشاء Firebase ضع Web App Config هنا. لا تضع Service Account أو private key.
+export const firebaseConfig={apiKey:'YOUR_API_KEY',authDomain:'YOUR_PROJECT.firebaseapp.com',projectId:'YOUR_PROJECT_ID',storageBucket:'YOUR_PROJECT.firebasestorage.app',messagingSenderId:'YOUR_SENDER_ID',appId:'YOUR_APP_ID'};
+export const OPENWEATHER_API_KEY='YOUR_OPENWEATHER_KEY';
